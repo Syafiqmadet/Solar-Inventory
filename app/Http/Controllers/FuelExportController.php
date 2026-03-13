6b87038c1ba3e96bfce577980755648d04b8e436
unknown <syafiqmadet@gmail.com>
2026-03-13 09:53:22 +0800

    Fix FuelExportController

--- app/Http/Controllers/FuelExportController.php
+++ app/Http/Controllers/FuelExportController.php
@@ -5,10 +5,12 @@ use App\Exports\FuelReportExport;
 use Maatwebsite\Excel\Facades\Excel;
 
 class FuelExportController extends Controller
+{
     use HasProjectScope;
+
     public function export()
     {
-        $pid = session('current_project_id');
+        $pid = $this->pid();
         if (!$pid) return back()->with('error', 'No project selected.');
         $filename = 'Fuel_Report_' . now()->format('Y-m-d') . '.xlsx';
         return Excel::download(new FuelReportExport($pid), $filename);
